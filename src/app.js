692fec1f84552386235730db216a163e4f528d15
Michele Lorusso <kirito102095@hotmail.it>
2021-05-13 15:35:41 +0200

    Exercise Complete ( error checking )

--- src/app.js
+++ src/app.js
@@ -3,6 +3,27 @@ var app = new Vue(
     el: '#root',
     data: {  
         navLink: [ 'Home', 'Pages', 'Courses', 'Features', 'Blog', 'shop'],
+        adress: [
+            '382 NE 191st ST # 87394 Miamim, FL 33179-3899',
+            '+1 (305)547-9909(9am-5pm EST, Monday, Friday)',
+            'support@maxcoach.com'
+        ],
+        explore: [
+            'Start here',
+            'Blog',
+            'About us',
+        ],
+        empty: [
+            'Success story',
+            'Courses',
+            'Contact Us',
+        ],
+        information: [
+            'Membership',
+            'Purchase Guide',
+            'Privacy Policy',
+            'Terms of Service',
+        ]
     }, 
     methods: {
 
